8dc8b285dcdf6e7267f91782181d7e9050d97528
xb205500 <edilson@jaya-apps.com>
2018-06-27 10:47:10 -0300

    First commit - consuming api to display photos on a component

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FotoModule } from './foto/foto.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CardModule } from './card/card.module';
+import { ListagemComponent } from './listagem/listagem.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ModuleWithProviders } from './app-routes';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListagemComponent,
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FotoModule,
+    CardModule,
+    ModuleWithProviders
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
